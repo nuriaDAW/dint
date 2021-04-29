557775f50c38e0e61a61fec6391324a3a0cedc96
Nuria Barrios Santos <nuria.barsan@educa.jcyl.es>
2021-04-29 22:08:23 +0200

    Actualizadas prácticas UT2

---
+++ ut2/practicas/introduccion-sass/materialize-sass/js/bin/init.js
@@ -0,0 +1,10 @@
+(function ($) {
+  $(function () {
+
+    $(".dropdown-trigger").dropdown();
+
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
